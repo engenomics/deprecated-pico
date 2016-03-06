650f6e3c27f7f66e00e3d24b9877d94f7dde94c1
AG8 <andrew2000g@gmail.com>
2016-03-05 19:35:02 -0500

    Finished lame compression (59%)
    No DFT or anything. Simply prints out the IDs where needed, instead of
    the replacement. Takes up 59% of the space needed for an analogous VCF
    file.

--- src/org/engenomics/pico/Main.java
+++ src/org/engenomics/pico/Main.java
@@ -1,7 +1,14 @@
 package org.engenomics.pico;
 
 import Utils.Utils;
+import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.transform.DftNormalization;
+import org.apache.commons.math3.transform.FastFourierTransformer;
+import org.apache.commons.math3.transform.TransformType;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -22,6 +29,12 @@ public class Main {
     public static String FILEPATH = REL_PATH + CHROMOSOME_REL_PATH + FILE_REL_PATH; //The entire file put together
 
 
+    public static int practicalGenomeLength;
+
+
+    FastFourierTransformer fastFourierTransformer = new FastFourierTransformer(DftNormalization.STANDARD); //There's also .UNITARY
+
+
 //    public static int LOGBASE2INTERVAL = 4; // log base 2 of number of data points that are in each transformation complex
 
 //    public static int INTERVAL = (int) Math.pow(2, LOGBASE2INTERVAL);
@@ -39,6 +52,10 @@ public class Main {
         List<Variation> variationsInsertions = new ArrayList<>();
         List<Variation> variationsDeletions = new ArrayList<>();
 
+        practicalGenomeLength = 0;
+
+        List<String> linesForComparisonVCF = new ArrayList<>();
+
         List<String> lines = Files.readAllLines(Paths.get(FILEPATH));
 
         for (String line : lines) {
@@ -46,6 +63,12 @@ public class Main {
             int position = Integer.parseInt(parts[1]);
             String original = parts[3];
             String replacement = parts[4];
+
+            linesForComparisonVCF.add(position + "\t" + original + "\t" + replacement);
+
+            if (position > practicalGenomeLength) { //Basically make practicalGenomeLength equal to the latest changed position in the genome. N comparisons is more effective than storing N large numbers.
+                practicalGenomeLength = position;
+            }
 
             if (replacement.length() > 1) { //Not an SNP
                 if (original.length() > 1) { //Deletion
@@ -64,17 +87,61 @@ public class Main {
             variationsSNP.add(currentVariation);
         }
 
+        practicalGenomeLength++;
 
         List<VariationType> variationTypeSNPs = makeFrequencyMap(variationsSNP);
         List<VariationType> variationTypeInsertions = makeFrequencyMap(variationsInsertions);
         List<VariationType> variationTypeDeletions = makeFrequencyMap(variationsDeletions);
 
-//        variationTypeSNPs.forEach(System.out::println);
-//        variationTypeInsertions.forEach(System.out::println);
-//        variationTypeDeletions.forEach(System.out::println);
+        Map<Integer, VariationType> variationTypeIDs = saveVariationsToMap(variationTypeSNPs, variationTypeInsertions, variationTypeDeletions);
+
+
+
+        // Do all of the Fourier stuff now
+
+        // Make an array of the variation IDs. The index of the variation in the array is the position; the value is the ID. Thus, nucleotides identical to the reference genome are recorded as -1.
+        double[] variationIDs = new double[Utils.getNextPowerOfTwo(practicalGenomeLength)];
+        Arrays.fill(variationIDs, -1);
+
+        int numberOfIDs = variationTypeIDs.size();
+
+        for (int id = 0; id < numberOfIDs; id++) {
+            VariationType variationType = variationTypeIDs.get(id);
+            List<Integer> positions = variationType.getPositions();
+
+            for (Integer position : positions) {
+                variationIDs[position] = id;
+            }
+        }
+
+
+        System.out.println("Variations ready. Starting Fourier transform.");
+
+        Complex[] result = fastFourierTransformer.transform(variationIDs, TransformType.FORWARD);
+
+        System.out.println("Saving results to file...");
+
+
+
+
+        /* SAVE STUFF TO FILES */
+
+
+        // Save the list of IDs to a file in a standardized format
+        saveVariationsToFile(variationTypeSNPs, variationTypeInsertions, variationTypeDeletions);
+
+        // Save the FFT result
+//        saveTransformToFile(result);
+
+        saveValuesToFile(variationIDs);
+
+        // Make a normal VCF file for comparison
+        saveListToFile(linesForComparisonVCF);
+
+        System.out.println("Complete!");
     }
 
-    List<VariationType> makeFrequencyMap(List<Variation> variations) {
+    private List<VariationType> makeFrequencyMap(List<Variation> variations) {
         List<VariationType> variationTypes = new ArrayList<>();
 
         //Make a frequency map
@@ -91,7 +158,7 @@ public class Main {
                 existingPositionsFrequencyPair.incrementFrequency();
                 frequencies.put(existingVariation, existingPositionsFrequencyPair);
             } else {
-                frequencies.put(v, new PositionsFrequencyPair(v.getPosition()));
+                frequencies.put(v.cloneWithoutPosition(), new PositionsFrequencyPair(v.getPosition()));
             }
         }
 
@@ -104,5 +171,88 @@ public class Main {
         Collections.sort(variationTypes);
 
         return variationTypes;
+    }
+
+    private Map<Integer, VariationType> saveVariationsToMap(List<VariationType> snps, List<VariationType> insertions, List<VariationType> deletions) {
+        Map<Integer, VariationType> variationTypeIDs = new HashMap<>();
+
+        int id = 0; //Currently ids go from 0 to the sum of the lists' lengths. TODO: Maybe go from -lengthSum/2 to +lengthSum/2?
+
+        for (VariationType snp : snps) {
+            variationTypeIDs.put(id, snp);
+            id++;
+        }
+
+        for (VariationType insertion : insertions) {
+            variationTypeIDs.put(id, insertion);
+            id++;
+        }
+
+        for (VariationType deletion : deletions) {
+            variationTypeIDs.put(id, deletion);
+            id++;
+        }
+
+        return variationTypeIDs;
+    }
+
+    private void saveVariationsToFile(List<VariationType> snps, List<VariationType> insertions, List<VariationType> deletions) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter("genome22.idlist", "UTF-8");
+
+
+        int id = 0; //Currently ids go from 0 to the sum of the lists' lengths. TODO: Maybe go from -lengthSum/2 to +lengthSum/2?
+
+        writer.println("<<<<<SNP IDs>>>>>");
+        for (VariationType snp : snps) {
+            writer.println(id + ":" + snp.getVariation().getBase() + ";");
+            id++;
+        }
+
+        writer.println("<<<<<Insertion IDs>>>>>");
+        for (VariationType insertion : insertions) {
+            writer.println(id + ":" + insertion.getVariation().getReplacement() + ";");
+            id++;
+        }
+
+        writer.println("<<<<<Deletion IDs>>>>>");
+        for (VariationType deletion : deletions) {
+            writer.println(id + ":" + deletion.getVariation().getOrig() + "|" + deletion.getVariation().getReplacement() + ";");
+            id++;
+        }
+
+
+        writer.close();
+    }
+
+    private void saveListToFile(List<String> values) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter("comparison.vcf", "UTF-8");
+
+        values.forEach(writer::println);
+
+        writer.close();
+    }
+
+    private void saveValuesToFile(double[] values) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter("genome22.pico", "UTF-8");
+
+        for (double d : values) {
+            if (d == -1) {
+                writer.print("^");
+                continue;
+            }
+            writer.print("<" + (int)d + ">");
+        }
+
+        writer.close();
+    }
+
+    private void saveTransformToFile(Complex[] complex) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter("genome22.pico", "UTF-8");
+
+        for (Complex c : complex) {
+            writer.println(Math.round(c.getReal()) + " " + Math.round(c.getImaginary()));
+        }
+
+        writer.close();
     }
 }
